464f2d8f8036a2184295cf40e39734a4f4d78be9
Adelani Oluwatumininu <tumininade@gmail.com>
2026-02-01 15:35:55 +0100

    Testing with postman

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
 
 const todoSchema = Joi.object({
-  task: Joi.string().min(3).required()
+  task: Joi.string().min(3).required(),
+  completed: Joi.boolean()
 });
 
 module.exports = { todoSchema };
